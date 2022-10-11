741b0f379979d613e0af7a3d5ed07a0474e910b9
ordinary-berries <gleb.eremin123@gmail.com>
2022-10-11 12:33:27 +0300

    Updates to logger.py: added check_log_folder function and _create_log_folder in Log class

--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
 import logging
 from logging import StreamHandler
 import sys
+import os
 
 from datetime import datetime
 
@@ -9,6 +10,8 @@ class Log:
     def __init__(self):
         timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
+        self.check_log_folder()
+
         logging.basicConfig(filename=f'./logs/{timestamp}-log.txt', level=logging.DEBUG,
                             format='[%(asctime)s: %(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         stdout_handler = StreamHandler(sys.stdout)
@@ -16,6 +19,16 @@ class Log:
                                                       datefmt='%Y-%m-%d %H:%M:%S'))
         self.logger = logging.getLogger()
         self.logger.addHandler(stdout_handler)
+
+    def check_log_folder(self):
+        if not os.path.exists('./logs'):
+            self._create_log_folder()
+
+    def _create_log_folder(self):
+        try:
+            os.mkdir('./logs')
+        except FileExistsError:
+            pass
 
     def info(self, message):
         self.logger.info(message)
